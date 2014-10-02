cf2f9b950d440ddc47cb8147ef978779037f5cd7
Andrew S. Jensen <ajensen@coderfoundry.com>
2014-10-02 15:20:41 -0400

    Updated folders and docs

---
+++ Master/1_BootStrapExercises/Exercises/Exercises/Models/TestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exercises.Models
+{
+    public class TestViewModel
+    {
+        public int x = 42;
+        public int[] y = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    }
+}
